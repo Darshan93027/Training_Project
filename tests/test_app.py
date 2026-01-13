5eef80b190ba8f0dfd77c97f38e1a610ecf6f056
Darshan93027 <contactdarshan07@gmail.com>
2026-01-13 10:56:07 +0530

    setp ci\cd

---
+++ tests/test_app.py
@@ -0,0 +1,35 @@
+# -------- App Logic --------
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+
+# -------- Test Cases --------
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_sub():
+    assert sub(5, 3) == 2
+
+def test_mul():
+    assert mul(2, 4) == 8
+
+def test_div():
+    assert div(10, 2) == 5
+
+
+
+def pytest_sessionfinish(session, exitstatus):
+    if exitstatus == 0:
+        with open("example.txt", "w") as f:
+            f.write("CI pipeline processed project successfully\n")
